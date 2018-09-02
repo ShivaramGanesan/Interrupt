604346fdd0c1f46a5f0d72871cc931a9e7915ec2
Gowtham Ganesan <gganesan2712@gmail.com>
2018-09-02 20:26:36 +0530

    php login

---
+++ app/src/main/java/com/example/msd/interrupt/AppConfig.java
@@ -0,0 +1,16 @@
+package com.example.msd.interrupt;
+
+/**
+ * Created by msd on 9/2/18.
+ */
+
+public class AppConfig {
+
+
+    // Server user login url
+    public static String URL_LOGIN = "http://192.168.1.6:8888/android_login_api/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://192.168.1.6:8888/android_login_api/register.php";
+
+}
---
+++ app/src/main/java/com/example/msd/interrupt/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.msd.interrupt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this, "Hello Shiva", Toast.LENGTH_SHORT).show();
+    }
+}
